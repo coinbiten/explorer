4d823f53730234f9240ab90fb631a1dbc362222c
Ansarul Mullah <mdansarul543@gmail.com>
2022-12-05 19:22:51 +0600

    okkk

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser');
+const path = require('path');
 const api = require("./routes/api");
 const route = require("./routes/route");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
 app.use("/",route);
 app.use("/api",api);
--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router();
-router.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+
+
+router.get('/', (request, response) => { 
+  response.render("index.ejs", {title: 'MCash Cryptocurrency'});
+})
   
 
 module.exports=router    
