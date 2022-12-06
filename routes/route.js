5c4459236e844e8d2b1f66397e74fb69b9d7298d
Ansarul Mullah <mdansarul543@gmail.com>
2022-12-07 01:29:21 +0600

    okkk

--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router();
-
+var config = require("../config/config")
 
 router.get('/', (request, response) => { 
-  response.render("index.ejs", {title: 'Biten Coin Explorer'});
+  response.render("index.ejs", {title: 'Biten Coin Explorer',name:config.coinName,symbol:config.symbol,networt_name:config.networt_name});
 })
   
 
