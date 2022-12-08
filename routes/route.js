2fbe92d3a65131b4552bbaf8d7be088218db432d
Ansarul Mullah <mdansarul543@gmail.com>
2022-12-08 23:35:26 +0600

    okkk

--- routes/route.js
+++ routes/route.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const req = require('express/lib/request');
 const router = express.Router();
 var config = require("../config/config")
 
@@ -7,17 +8,26 @@ router.get('/', (request, response) => {
 })
   
 router.get('/blocks', (request, response) => { 
-  response.render("blocks.ejs", {title: config.networt_name+" | All Blocks",name:config.coinName,symbol:config.symbol,networt_name:config.networt_name});
+  response.render("blocks.ejs", {title: config.networt_name+"  All Blocks",name:config.coinName,symbol:config.symbol,networt_name:config.networt_name});
 })
 
 router.get('/txs', (request, response) => { 
-  response.render("alltrx.ejs", {title: config.networt_name+" | All Transactions",name:config.coinName,symbol:config.symbol,networt_name:config.networt_name});
+  response.render("alltrx.ejs", {title: config.networt_name+"  All Transactions",name:config.coinName,symbol:config.symbol,networt_name:config.networt_name});
 })
 
 router.get('/txsPending', (request, response) => { 
-  response.render("pendingtrx.ejs", {title: config.networt_name+" | Pending Transactions",name:config.coinName,symbol:config.symbol,networt_name:config.networt_name});
+  response.render("pendingtrx.ejs", {title: config.networt_name+"  Pending Transactions",name:config.coinName,symbol:config.symbol,networt_name:config.networt_name});
 })
 
+router.get('/block/:id', (request, response) => { 
+  var id = request.params.id
+  response.render("blockview.ejs", {title: config.networt_name+"  Blocks #"+id ,block:id,name:config.coinName,symbol:config.symbol,networt_name:config.networt_name});
+})
+
+router.get('/tx/:hash', (request, response) => { 
+  var hash = request.params.hash
+  response.render("trx.ejs", {title: config.networt_name+" Transaction Hash (TxHash) " ,hash:hash,name:config.coinName,symbol:config.symbol,networt_name:config.networt_name});
+})
 
 
 
